228a3cba76e4d4f558e3397ac29f0640bf1cb3d3
bo4crt <bo4crt@greatermanchester.ac.uk>
2026-01-20 14:10:20 +0000

    Merge remote-tracking branch 'origin/master'

--- Assignment.py
+++ Assignment.py
@@ -24,53 +24,39 @@ quiz = [{"question": "What colour is the sky?",
 
 
 def check_answer(correct_answer, user_input):
-    if user_input == correct_answer:
-        return True
-    else:
-        return False
+    return correct_answer == user_input
 
-import random
 
 def run_quiz():
-    question_counter = 0
     score = 0
     total_questions = len(quiz)
 
-    while question_counter < total_questions:
+    random.shuffle(quiz)
 
-        random.shuffle(quiz)
+    for question in quiz:
+        print("\n" + question["question"])
 
-        for question in quiz:
-            print("\n" + question["question"])
+        random.shuffle(question["options"])
 
-            for i in range(len("options"[question_counter])):
-                print(i + 1, "options"[question_counter][i])
+        for i in range(len(question["options"])):
+            print(i + 1, question["options"][i])
 
-            user_choice = int(input("\nEnter your choice: ")) - 1
-            user_answer = question["options"][user_choice]
+        user_choice = int(input("Enter your choice: ")) - 1
+        user_answer = question["options"][user_choice]
 
-            if check_answer(question["answer"], user_answer):
-                print("Correct!")
-                score += 1
-            else:
-                print("Incorrect!")
+        if check_answer(question["answer"], user_answer):
+            print("Correct!")
+            score += 1
+        else:
+            print("Incorrect!")
 
-        print("\nEnd of quiz.")
+    print("\nEnd of quiz.")
     print(f"Your score is {score}/{total_questions}")
     print("You have scored", score / total_questions * 100, "% on this quiz.")
 
 
-# run_quiz()
+run_quiz()
 
 
-q = {"question": "What colour is blood?",
-            "options": ["Red", "Blue", "Green", "Purple"],
-            "answer": "Red"}
-print(q)
-# shuffle options
-random.shuffle(q["options"])
-print(q["options"])
-# find index position of answer after shuffle
-# use this position to match user input to stored answer
-answer = q["answer"]
-print(q["options"].index(answer))
+
+
